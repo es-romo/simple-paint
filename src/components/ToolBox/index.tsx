b911ae60dfbeedd587da1ab47b704a292ceb9f26
Esteban Romo <esteban.romo.98@gmail.com>
2021-03-21 14:46:44 -0400

    Add change dimensions functionality

--- src/components/ToolBox/index.tsx
+++ src/components/ToolBox/index.tsx
@@ -1,10 +1,12 @@
 import './ToolBox.css'
 import ColorPicker from './ColorPicker'
+import Dimensions from './Dimensions'
 
 function ToolBox() {
     return(
         <div className='ToolBox'>
             <ColorPicker/>
+            <Dimensions/>
         </div>
     )
 }
